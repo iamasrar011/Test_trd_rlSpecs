1569080d1dae8ffd43b62fdd68abc48c1d2e03e1
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-09-19 08:03:40 +0530

    Create portfolio_widget.dart

---
+++ Widgets/portfolio_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PortfolioWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement portfolio widget UI here
+  }
+}
