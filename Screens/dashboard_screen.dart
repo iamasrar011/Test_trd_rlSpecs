84913d78fa1e5442ddd46be10b98a52a7e74810f
krystopher <116830845+krydsx@users.noreply.github.com>
2023-09-19 08:14:13 +0530

    Update dashboard_screen.dart

--- Screens/dashboard_screen.dart
+++ Screens/dashboard_screen.dart
@@ -4,5 +4,23 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Implement your dashboard screen UI here
+        return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Your Dashboard',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            // Display user portfolio information and widgets here
+          ],
+        ),
+      ),
+    );
   }
 }
