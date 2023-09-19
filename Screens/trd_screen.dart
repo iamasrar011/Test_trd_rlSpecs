8af3135d46bc2a6f5462b204902d9410ef8a163f
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-09-19 07:56:39 +0530

    Create trd_screen.dart

---
+++ Screens/trd_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TradeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement your trade screen UI here
+  }
+}
