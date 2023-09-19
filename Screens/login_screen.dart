b7d2160240a584479c5e3325c48e87c38e4b04f9
krystopher <116830845+krydsx@users.noreply.github.com>
2023-09-19 11:29:24 +0530

    Update login_screen.dart

--- Screens/login_screen.dart
+++ Screens/login_screen.dart
@@ -3,22 +3,43 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Implement your login screen UI here
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Algorithmic Trading!',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            // Add login form fields and buttons here
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to Algorithmic Trading!',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement login logic here
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => DashboardScreen(),
+                    ),
+                  );
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
