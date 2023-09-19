937f956f52d7ab5865210f97eb7f1b5398976931
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-09-19 08:04:08 +0530

    Create trading_form_widget.dart

---
+++ Widgets/trading_form_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TradingFormWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement trading form widget UI here
+  }
+}
