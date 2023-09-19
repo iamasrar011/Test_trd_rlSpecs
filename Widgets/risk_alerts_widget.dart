732e0be9e257ec34acac187d3ab466e633e93334
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-09-19 08:04:31 +0530

    Create risk_alerts_widget.dart

---
+++ Widgets/risk_alerts_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RiskAlertsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement risk alerts widget UI here
+  }
+}
