ba1344b415b9cadf64fac15e1a9e64eb46a708e8
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-10-04 17:50:31 +0530

    Update Strat.dart

--- Models/Strat.dart
+++ Models/Strat.dart
@@ -1,3 +1,87 @@
+// strat.dart
+
 class Strategy {
-  // Implement strategy data fields and methods here
+  final String name;
+  final String description;
+  final double riskLevel;
+  final List<String> technicalIndicators; // List of technical indicators used
+  final List<String> tradedAssets;        // List of assets the strategy trades
+
+  Strategy({
+    required this.name,
+    required this.description,
+    required this.riskLevel,
+    required this.technicalIndicators,
+    required this.tradedAssets,
+  });
+
+  // Method to execute the trading strategy
+  void executeStrategy() {
+    // Implement your algorithmic trading logic here
+    print('Executing trading strategy: $name');
+    print('Risk Level: $riskLevel');
+    print('Technical Indicators: ${technicalIndicators.join(', ')}');
+    print('Traded Assets: ${tradedAssets.join(', ')}');
+
+    // Example: Simulated historical price data (replace with actual data source)
+    final List<double> assetPrices = getHistoricalPrices();
+
+    if (assetPrices.length < 2) {
+      print('Insufficient data for trading strategy.');
+      return;
+    }
+
+    // Example: Calculate a simple moving average (SMA) for a given period
+    final int smaPeriod = 10;
+    final double movingAverage = calculateMovingAverage(assetPrices, smaPeriod);
+
+    // Example: Determine the current position (buy/sell/hold) based on the SMA
+    final double lastPrice = assetPrices.last;
+    final double currentPosition = determinePosition(lastPrice, movingAverage);
+
+    // Example: Implement trading logic based on the position
+    if (currentPosition == 1) {
+      print('Signal: Buy');
+      // Implement buy order logic here
+    } else if (currentPosition == -1) {
+      print('Signal: Sell');
+      // Implement sell order logic here
+    } else {
+      print('Signal: Hold');
+      // Implement hold or do nothing logic here
+    }
+  }
+
+  // Additional methods and properties can be added as needed
+
+  // Example method to get historical price data (replace with actual data source)
+  List<double> getHistoricalPrices() {
+    // Simulated historical price data
+    return [100.0, 105.0, 110.0, 115.0, 120.0, 125.0, 130.0, 135.0, 140.0, 145.0, 150.0, 155.0, 160.0];
+  }
+
+  // Example method to calculate a simple moving average (SMA)
+  double calculateMovingAverage(List<double> prices, int period) {
+    if (prices.length < period) {
+      return 0.0; // Insufficient data for calculation
+    }
+
+    double sum = 0;
+    for (int i = prices.length - period; i < prices.length; i++) {
+      sum += prices[i];
+    }
+
+    return sum / period;
+  }
+
+  // Example method to determine the current position based on SMA values
+  double determinePosition(double currentPrice, double movingAverage) {
+    if (currentPrice > movingAverage) {
+      return 1; // Buy signal
+    } else if (currentPrice < movingAverage) {
+      return -1; // Sell signal
+    } else {
+      return 0; // Hold or do nothing
+    }
+  }
 }
