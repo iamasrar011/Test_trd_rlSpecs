67b23879460d5473b76b07a8607914099ad33fe1
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-10-04 17:57:33 +0530

    Update user.dart

--- Models/user.dart
+++ Models/user.dart
@@ -1,3 +1,33 @@
 class User {
-  // Implement user data fields and methods here
+  final String username;
+  final String email;
+  final double accountBalance;
+  final List<String> watchlist;
+
+  User({
+    required this.username,
+    required this.email,
+    required this.accountBalance,
+    required this.watchlist,
+  });
+
+  void addToWatchlist(String asset) {
+    watchlist.add(asset);
+  }
+
+  void removeFromWatchlist(String asset) {
+    watchlist.remove(asset);
+  }
+
+  void deposit(double amount) {
+    accountBalance += amount;
+  }
+
+  void withdraw(double amount) {
+    if (amount <= accountBalance) {
+      accountBalance -= amount;
+    } else {
+      print('Insufficient funds for withdrawal.');
+    }
+  }
 }
