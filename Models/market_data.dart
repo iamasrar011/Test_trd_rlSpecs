d22e23a1eb0a0e8cc38920915076aa50c787f2ec
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-10-04 17:55:05 +0530

    Update market_data.dart

--- Models/market_data.dart
+++ Models/market_data.dart
@@ -1,3 +1,29 @@
+// market_data.dart
+
+import 'dart:async';
+import 'dart:math';
+
 class MarketData {
-  // Implement market data fields and methods here
+  // Simulated market data for demonstration purposes
+  final List<String> availableAssets = ['AAPL', 'GOOGL', 'TSLA', 'AMZN', 'MSFT'];
+  final Map<String, double> latestPrices = {}; // Store latest prices for assets
+
+  // Function to simulate fetching latest prices for available assets
+  Future<void> fetchLatestPrices() async {
+    for (final asset in availableAssets) {
+      final double price = _simulatePriceChange(asset);
+      latestPrices[asset] = price; // Store the latest price for the asset
+      await Future.delayed(Duration(seconds: 1)); // Simulate fetching delay
+    }
+  }
+
+  // Function to simulate price change for an asset
+  double _simulatePriceChange(String asset) {
+    final double currentPrice = latestPrices[asset] ?? 100.0; // Initial price
+    final double volatility = 0.2; // Simulated volatility
+    final double randomFactor = Random().nextDouble() * 0.1 - 0.05; // Random price change
+
+    final double newPrice = currentPrice * (1 + volatility * randomFactor);
+    return newPrice;
+  }
 }
