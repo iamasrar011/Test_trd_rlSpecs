c42a0787b0114568569fb28000ed259dc73eeabf
Asrar <103955110+iamasrar011@users.noreply.github.com>
2023-10-04 18:07:45 +0530

    Update trading_form_widget.dart

--- Widgets/trading_form_widget.dart
+++ Widgets/trading_form_widget.dart
@@ -1,8 +1,51 @@
+// trading_form_widget.dart
+
 import 'package:flutter/material.dart';
 
 class TradingFormWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Implement trading form widget UI here
+    return Container(
+      // Trading form UI components go here
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Trading form title
+          Text(
+            'Trade Form',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16), // Spacer
+
+          // Input fields for trading form
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Asset Symbol'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Quantity'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Price'),
+          ),
+          // Add more input fields for order types, stop-loss, etc., as needed
+
+          SizedBox(height: 16), // Spacer
+
+          // Trade execution button
+          ElevatedButton(
+            onPressed: () {
+              // Implement trade execution logic here
+            },
+            child: Text('Execute Trade'),
+          ),
+        ],
+      ),
+    );
   }
 }
+
